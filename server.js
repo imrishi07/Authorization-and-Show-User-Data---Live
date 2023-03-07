455dff1e1df019cf4992087bbba75b7feed92823
Rishi Anand <67603086+imrishi07@users.noreply.github.com>
2023-03-07 19:32:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Connect Database
 connectDB();
@@ -15,6 +21,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
